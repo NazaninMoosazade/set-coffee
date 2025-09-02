15b6d66a2b0509b43d4229ffd596b065a876761c
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-09-02 13:21:34 +0330

    create Login.js

---
+++ src/app/login/register/page.js
@@ -0,0 +1,19 @@
+const login_register = () => {
+  return (
+    <div>
+      <div  data-aos="fade-up">
+        {/* 
+          <Login showRegisterForm={showRegisterForm} />
+          <Register showloginForm={showloginForm} /> */}
+      </div>
+      <section>
+        <img
+          src="https://neurosciencenews.com/files/2023/06/coffee-brain-caffeine-neuroscincces.jpg"
+          alt=""
+        />
+      </section>
+    </div>
+  );
+};
+
+export default login_register;
