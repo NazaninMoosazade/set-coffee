15ce716c221de7f337d44674e3f4b8ab85bcdb40
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-09-03 12:34:57 +0330

    create db.js

--- src/app/forget-password/page.js
+++ src/app/forget-password/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const ForgotPassword = () => {
   return (
-    <div className="z-[999] h-screen w-full flex overflow-hidden bg-[rgb(237,230,234)] bg-cover bg-fixed">
+    <div className="z-[999] min-h-screen w-full flex flex-col md:flex-row overflow-hidden bg-[rgb(237,230,234)]">
       {/* سمت فرم */}
-      <div data-aos="fade-up" className="w-1/2 flex flex-col justify-center">
-        <div className="grid bg-white p-6 w-[380px] mx-auto mt-52 mb-8 rounded-md shadow-md text-center text-black">
+      <div className="md:w-1/2 flex flex-col justify-center px-4 md:px-0">
+        <div className="grid bg-white p-6 w-full max-w-[380px] mx-auto mt-24 md:mt-52 mb-8 rounded-md shadow-md text-center text-black">
           <input
             type="text"
             placeholder="ایمیل / شماره موبایل"
@@ -16,14 +16,14 @@ const ForgotPassword = () => {
             بازنشانی رمزعبور
           </button>
           <Link
-            href="/login-register"
+            href="/login/register"
             className="text-sm cursor-pointer text-center mt-4 mb-12"
           >
             برگشت به ورود
           </Link>
         </div>
         <Link
-          href="/login-register"
+          href="/login/register"
           className="block w-max mx-auto font-shabnam cursor-pointer text-sm text-[rgb(52,24,14)]"
         >
           لغو
@@ -31,7 +31,7 @@ const ForgotPassword = () => {
       </div>
 
       {/* سمت تصویر */}
-      <section className="w-1/2 h-full bg-[rgb(52,24,14)]">
+      <section className="md:w-1/2 h-64 md:h-full bg-[rgb(52,24,14)]">
         <img
           src="https://neurosciencenews.com/files/2023/06/coffee-brain-caffeine-neuroscincces.jpg"
           alt=""
