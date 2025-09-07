8be7587568f3bedc87899cb1aa0c9a82f28450cb
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-09-07 14:18:01 +0330

    coding navbar.js

--- src/Components/modules/Navbar/Navbar.js
+++ src/Components/modules/Navbar/Navbar.js
@@ -4,8 +4,10 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaShoppingCart, FaRegHeart } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io"
 
-export default function Navbar() {
+
+export default function Navbar({ isLogin }) {
   const [fixTop, setFixTop] = useState(false);
 
   useEffect(() => {
@@ -57,24 +59,31 @@ export default function Navbar() {
           <li>
             <Link href={"/roles"}>قوانین</Link>
           </li>
-          <li>
-            <Link href={"/login/register"}>ورود / عضویت</Link>
-          </li>
 
-          {/* Start My-account section */}
-          {/* <div>
-            <Link href="/p-user">
-              <IoIosArrowDown />
-              حساب کاربری
-            </Link>
-            <div>
-              <Link href="/p-user/orders">سفارشات</Link>
-              <Link href="/p-user/tickets">تیکت های پشتیبانی</Link>
-              <Link href="/p-user/comments">کامنت‌ها</Link>
-              <Link href="/p-user/wishlist">علاقه‌مندی‌ها</Link>
-              <Link href="/p-user/account-details">جزئیات اکانت</Link>
-            </div>
-          </div> */}
+          {isLogin ? (
+            <>
+              {/* Start My-account section */}
+<div className="relative group">
+  <Link className="flex items-center gap-x-1.5 cursor-pointer" href="/p-user">
+    <IoIosArrowDown className="p-0" />
+    حساب کاربری
+  </Link>
+
+  <div className="absolute z-50 right-0 mt-2 w-48 bg-white border rounded shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-300">
+    <Link className="block px-4 py-2 hover:bg-gray-100" href="/p-user/orders">سفارشات</Link>
+    <Link className="block px-4 py-2 hover:bg-gray-100" href="/p-user/tickets">تیکت های پشتیبانی</Link>
+    <Link className="block px-4 py-2 hover:bg-gray-100" href="/p-user/comments">کامنت‌ها</Link>
+    <Link className="block px-4 py-2 hover:bg-gray-100" href="/p-user/wishlist">علاقه‌مندی‌ها</Link>
+    <Link className="block px-4 py-2 hover:bg-gray-100" href="/p-user/account-details">جزئیات اکانت</Link>
+  </div>
+</div>
+
+            </>
+          ) : (
+            <li>
+              <Link href={"/login/register"}>ورود / عضویت</Link>
+            </li>
+          )}
 
           {/* Finish My-account section */}
         </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,32 @@ import Banner from "@/Components/templates/index/banner/Banner";
 import Latest from "@/Components/templates/index/latest/latest";
 import Promote from "@/Components/templates/index/promote/Promote";
 import Footer from "@/Components/modules/Footer/Footer";
+import { verifyAccessToken } from "@/utils/auth";
+import { cookies } from "next/headers";
+import UserModel from "@/models/User";
+import connectToDB from "@/configs/db";
 
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  await connectToDB();
+  const token = cookies().get("token");
+  let user = null;
+
+  if (token) {
+    const tokenPayload = verifyAccessToken(token.value);
+    if (tokenPayload) {
+      user = await UserModel.findOne({ email: tokenPayload.email });
+    }
+  }
   return (
     <>
-      <Navbar />
-      <Banner/>
-      <Latest/>
-      <Promote/>
-      <Articles/>
-      <Footer/>
+      <Navbar isLogin={user} />
+      <Banner />
+      <Latest />
+      <Promote />
+      <Articles />
+      <Footer />
     </>
   );
 }
